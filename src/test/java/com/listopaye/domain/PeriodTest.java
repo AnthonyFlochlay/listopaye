c2a0527b51efff5657f252b9c72105f7ba8015c1
Anthony Flochlay <anthony.flochlay@gmail.com>
2023-09-29 09:50:23 +0200

    Big reset

--- src/test/java/com/listopaye/domain/PeriodTest.java
+++
@@ -1,5 +0,0 @@
-package com.listopaye.domain;
-
-public interface PeriodTest extends StartsBeforeOrEqualTimeTest, EndsBeforeOrEqualTimeTest, StartsBeforeOrEqualPeriodTest, EndsAfterOrEqualTest, IsFullyIncludedTest {
-
-}
